97bba60f8a852c1666cd75da906f3cdd5644a93d
mustafahashi <44781281+mustafahashi@users.noreply.github.com>
2023-01-26 22:17:47 +0300

    Update login.cs

--- DTO/login.cs
+++ DTO/login.cs
@@ -12,6 +12,8 @@ namespace pharmacy.DTO
         public int id { get; set; }
 
         public string username { get; set; }
+        //not importat
+        public string IsSignedIn { get; set; }
    
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
 
